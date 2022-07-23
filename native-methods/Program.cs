3fa5c52a25414ecb4e8096b281240d3aaff79221
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-23 00:36:17 -0300

    ComparateTo and toString

---
+++ native-methods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace native_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           object obj = new object();
+            Console.WriteLine(obj.ToString());
+        }
+    }
+}
