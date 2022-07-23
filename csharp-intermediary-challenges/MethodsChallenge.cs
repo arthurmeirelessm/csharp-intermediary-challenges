d6015dce8eefa550f3b7ea257a20e7ba6538c0ba
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-23 01:50:55 -0300

    List tests

---
+++ csharp-intermediary-challenges/MethodsChallenge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_intermediary_challenges
+{
+    public class MethodsChallenge
+    {
+        public void run()
+        {
+            Console.WriteLine("oi ola"); ;
+        }
+    }
+}
