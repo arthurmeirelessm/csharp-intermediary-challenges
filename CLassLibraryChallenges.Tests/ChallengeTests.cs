93ca952a1fa23787da210a92ef7ca1f9f7b53f6b
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-28 01:32:53 -0300

    [ERROR] Method of test in classLibrary contains one error

--- CLassLibraryChallenges.Tests/ChallengeTests.cs
+++ CLassLibraryChallenges.Tests/ChallengeTests.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace CLassLibraryChallenges.Tests
 {
     public class ChallengeTests
     {
+        [TestFixture]
+
+        public class MethodsChallenge
+        {
+            [Test]
+
+            public void FullNameMonthTest()
+            {
+                var sut = new MethodsChallenge();
+
+                var response = sut.FullNameMonth(4);
+
+
+            }
+
+        }
+
     }
 }
